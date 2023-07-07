033214eb434d2d0a7dc1c84697038e231996f642
yarocruz <yarocruz@gmail.com>
2023-07-07 10:46:07 -0400

    hooked up with OpenAI

--- index.js
+++ index.js
@@ -1,3 +1,32 @@
 require("dotenv").config();
+const { OpenAI } = require("langchain/llms/openai");
+const inquirer = require("inquirer");
 
-console.log(process.env.OPENAI_API_KEY);
+const model = new OpenAI({
+    openAIApiKey: process.env.OPENAI_API_KEY,
+    temperature: 0,
+    model: 'gpt-3.5-turbo'
+})
+
+const prompt = async (input) => {
+    try {
+        const res = await model.call(input);
+        console.log(res)
+    } catch (err) {
+        console.log(err);
+    }
+}
+
+const init = () => {
+    inquirer.prompt([
+        {
+            type: 'input',
+            name: 'name',
+            message: 'Ask a coding question:',
+        },
+    ]).then((res) => {
+        prompt(res.name)
+    })
+}
+
+init()
